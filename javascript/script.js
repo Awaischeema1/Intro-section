6dc106e8f7dc9bb724250ef4c95bc524cd09ff40
Awais <awaischeema201@gmail.com>
2022-08-12 21:24:17 +0500

    reponsive menu

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+const dropdownMenus = document.querySelectorAll('.dropdown-menu'),
+dropdownBtns = document.querySelectorAll('.down-arrow'),
+menuToggleBtn = document.querySelector('.toggle-btn'),
+mobileMenu = document.querySelector('.main-menu'),
+menuCloseBtn = document.querySelector('.close-btn');
+
+dropdownBtns.forEach((dropdownBtn,index)=>{
+    dropdownBtn.addEventListener('click',(e)=>{
+        dropdownMenus[index].classList.toggle('active');
+        e.target.classList.toggle('open');
+    })
+})
+menuToggleBtn.addEventListener('click',()=>{
+    mobileMenu.classList.add('active');
+})
+menuCloseBtn.addEventListener('click',()=>{
+    mobileMenu.classList.remove('active');
+})
